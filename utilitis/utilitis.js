16142a5bd02846636c1d233a1222b5357e2a2c5a
tushar <iktushar2020@gmail.com>
2025-02-28 08:20:46 +0600

    added addMoney.js functionalities

--- utilitis/utilitis.js
+++ utilitis/utilitis.js
@@ -1,3 +1,21 @@
 function handleToggle(id, status){
     document.getElementById(id).style.display = status;
 }
+
+function getInputValueById(id) {
+    const value = document.getElementById(id).value;
+    const convertedValue = parseFloat(value);
+    return convertedValue;
+}
+
+function getInnerTextById(id) {
+    const value = document.getElementById(id).innerText;
+    const convertedValue = parseFloat(value);
+    return convertedValue;
+}
+
+function setInnerTextByIdAndValue(id, value){
+    document.getElementById(id).innerText = value;
+    document.getElementById(id).value = value;
+    return value;
+}
