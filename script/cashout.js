cea1e72e8896dcd05642a7571acf356343129e2f
tushar <iktushar2020@gmail.com>
2025-02-28 12:56:01 +0600

    added transaction system

--- script/cashout.js
+++ script/cashout.js
@@ -5,6 +5,32 @@ document.getElementById("out_money_btn").addEventListener("click", function(even
     const accountNumber = document.getElementById("user_number").value;
     const mainBalance = getInnerTextById("main_balance");
 
+
+
+
+
+    const transactionContainer = document.getElementById("transaction_container");
+    const newTransaction = document.createElement("div");
+    newTransaction.classList.add("flex", "items-center", "gap-3", "mt-2" , "mb-2");
+  
+    const now = new Date();
+        const formattedTime = now.toLocaleString();
+  
+        newTransaction.innerHTML = `
+        <div class="flex items-center gap-3 mb-6">
+        <img class="w-12 p-1 bg-slate-200 rounded-full" src="./resources/send.svg" alt="">
+        <div>
+          <h1 class="font-medium text-base">Cash Out</h1>
+          <p>Cash Out $ ${amount} Form ${accountNumber}</p>
+          <p class="text-gray-500 text-sm">${formattedTime}</p>
+        </div>
+        </div>
+      `;
+  
+      transactionContainer.appendChild(newTransaction);
+  
+
+
     if (
         accountNumber.length === 11 &&
         accountNumber.slice(0, 2) === "01" &&
--- script/transfer.js
+++ script/transfer.js
@@ -5,6 +5,29 @@ document.getElementById("tranfer_money_btn").addEventListener("click", function(
     const accountNumber = document.getElementById("reciver_number").value;
     const mainBalance = getInnerTextById("main_balance");
 
+
+    const transactionContainer = document.getElementById("transaction_container");
+    const newTransaction = document.createElement("div");
+    newTransaction.classList.add("flex", "items-center", "gap-3", "mt-2" , "mb-2");
+  
+    const now = new Date();
+        const formattedTime = now.toLocaleString();
+  
+        newTransaction.innerHTML = `
+        <div class="flex items-center gap-3 mb-6">
+        <img class="w-12 p-1 bg-slate-200 rounded-full" src="./resources/send.svg" alt="">
+        <div>
+          <h1 class="font-medium text-base">Transfer Money</h1>
+          <p>Transfer $ ${amount} To ${accountNumber}</p>
+          <p class="text-gray-500 text-sm">${formattedTime}</p>
+        </div>
+        </div>
+      `;
+  
+      transactionContainer.appendChild(newTransaction);
+  
+
+
     if (
         accountNumber.length === 11 &&
         accountNumber.slice(0, 2) === "01" &&
